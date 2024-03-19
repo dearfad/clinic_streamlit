9f490b1216a50de169044fbe5808a81b268dc468
dearfad <dearfad@sina.com>
2024-03-19 21:53:10 +0800

    update

--- webui_streamlit.py
+++ webui_streamlit.py
@@ -8,8 +8,15 @@ st.set_page_config(
     menu_items=None,
 )
 
-st.markdown(" <style> div[class^='block-container'] { padding-top: 2rem; } </style> ", unsafe_allow_html=True)
-
-st.header("👩 病例分析", divider='red')
+st.markdown("<style>div[class^='block-container']{padding-top:2rem;}</style>", unsafe_allow_html=True)
+st.subheader("📄 病例分析", divider='rainbow')
 st.caption("吉林大学中日联谊医院乳腺外科")
-st.write('**你是一名乳腺外科医生，现在用平常谈话的方法跟眼前的患者沟通，请尝试做出你的诊断。**')
+st.write('>**作为一名乳腺外科医生，与患者正在进行沟通，请尝试做出你的诊断。**')
+
+with st.container(border=True):
+    col1, col2 = st.columns([1,3], gap='large')
+    with col1:
+        st.image('dearfad.png')
+    with col2:
+        st.write('**姓名**：王淑芬')
+        st.write('**年龄**：39岁')
