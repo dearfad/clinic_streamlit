df41561a238e53d0e6229c8761ac58303d1b0b90
dearfad <dearfad@gmail.com>
2024-08-08 08:17:31 +0800

    update

--- pages/admin.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from libs.bvcpage import set_page_header
-
-set_page_header()
-
-st.markdown(":material/admin_panel_settings: **管理员**")
-
-if st.button("返回首页", use_container_width=True):
-    st.switch_page("breast.py")
