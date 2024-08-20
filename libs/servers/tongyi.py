d8d608f3707f258c672366449be7fa721ba20210
dearfad <dearfad@gmail.com>
2024-08-20 18:10:00 +0800

    Update tongyi.py

--- libs/servers/tongyi.py
+++ libs/servers/tongyi.py
@@ -73,11 +73,14 @@ class XingChen:
 
     def get_chat_histories(self, character_id, user_id, session_id) -> list:
         body = ChatHistoryQueryDTO(
+            # where=ChatHistoryQueryWhere(
+            #     characterId=character_id, bizUserId=user_id, sessionId=session_id
+            # ),
             where=ChatHistoryQueryWhere(
-                characterId=character_id, bizUserId=user_id, sessionId=session_id
+                characterId=character_id, bizUserId=user_id
             ),
-            orderBy=["gmtCreate asc"],
-            pageNum=10,
+            orderBy=["gmtCreate desc"],
+            pageNum=1,
             # pageSize=10,
         )
         result = self.chat_message_api.chat_histories(chat_history_query_dto=body)
@@ -93,7 +96,7 @@ xingchen = XingChen(api_key="lm-bw72h4Q9oFOyuE47ncPxbg==")
 # r = xingchen.chat(character_id="37d0bb98a0194eefbecdba794fb1b42c", message="那里不舒服？", seed=12345, user_id="123456789")
 # st.write(r)
 
-h = xingchen.get_chat_histories(character_id="37d0bb98a0194eefbecdba794fb1b42c",user_id="123456789", session_id="a020595ed022479bbad269ad69594382")
+h = xingchen.get_chat_histories(character_id="37d0bb98a0194eefbecdba794fb1b42c",user_id="1234567890", session_id="a020595ed022479bbad269ad69594382")
 st.write(h)
 
 # x = xingchen.reset_chat_history(character_id="37d0bb98a0194eefbecdba794fb1b42c",user_id="123456789")
