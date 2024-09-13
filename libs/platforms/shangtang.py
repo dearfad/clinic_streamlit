b8b8d0bac281006902ec3019f64cdbd46495cdca
dearfad <dearfad@gmail.com>
2024-09-13 21:13:17 +0800

    update

--- libs/platforms/shangtang.py
+++ libs/platforms/shangtang.py
@@ -1,10 +1,11 @@
 import sensenova
 import streamlit as st
-
-sensenova.access_key_id = st.secrets['shangtang_access_key_id']
-sensenova.secret_access_key = st.secrets['shangtang_secret_access_key']
+from libs.bvcconst import TOOLS
 
 def chat(patient):
+
+    sensenova.access_key_id = st.secrets['shangtang_access_key_id']
+    sensenova.secret_access_key = st.secrets['shangtang_secret_access_key']
     model = patient.model.name
     messages = patient.messages
     response = sensenova.ChatCompletion.create(
@@ -14,6 +15,13 @@ def chat(patient):
             # repetition_penalty=1.05,
             # temperature=0.8,
             # top_p=0.7,
+            stream=False,
+            tools=TOOLS,
         )
     print(response)
-    return response
+    if response.data.choices[0].get("tool_calls", None):
+        tool_call = response.data.choices[0].tool_calls[0]
+        args = eval(tool_call.function.arguments)
+        if tool_call.function.name == "get_report":
+            return f"好的，这是我的{args['report']} ![](app/static/乳腺超声.jpg)"
+    return response.data.choices[0].message
